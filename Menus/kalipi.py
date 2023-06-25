d9ae165fcf03ea4f2718fe2cc635ad820080f95b
aj-9636 <anje_knottnerus@hotmail.com>
2023-06-25 10:41:45 +0200

    try no frame

--- Menus/kalipi.py
+++ Menus/kalipi.py
@@ -218,7 +218,7 @@ def screen():
     else:
         pygame.mouse.set_visible(0)
         
-    screen.canvas = pygame.display.set_mode(size)
+    screen.canvas = pygame.display.set_mode(size, pygame.NOFRAME)
 
     # Background Color
     screen.canvas.fill(black)
