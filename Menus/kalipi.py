7c5601cf97f007f9017ddc804999db4f35d5072a
aj-9636 <anje_knottnerus@hotmail.com>
2023-07-01 11:52:25 +0200

    rm full screen?

--- Menus/kalipi.py
+++ Menus/kalipi.py
@@ -218,7 +218,7 @@ def screen():
     else:
         pygame.mouse.set_visible(0)
         
-    screen.canvas = pygame.display.set_mode(size, pygame.FULLSCREEN)
+    screen.canvas = pygame.display.set_mode(size)
 
     # Background Color
     screen.canvas.fill(black)
